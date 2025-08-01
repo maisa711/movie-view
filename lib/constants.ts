4e5c507cf41cd7cbcb1613cbc854590024f1b055
Giorgi Maisashvili <maisashvili.giorgi9@gmail.com>
2025-08-01 14:54:16 +0200

    starting project

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_THEME = {
+  light: {
+    background: 'hsl(249 53% 98%)', // --background
+    border: 'hsl(249 14% 90%)', // --border
+    card: 'hsl(249 53% 98%)', // --card
+    notification: 'hsl(5 83% 43%)', // --destructive
+    primary: 'hsl(249 100% 4%)', // --primary
+    text: 'hsl(249 51% 2%)', // --foreground
+  },
+  dark: {
+    background: 'hsl(249 55% 2%)', // --background (dark)
+    border: 'hsl(249 14% 15%)', // --border (dark)
+    card: 'hsl(249 55% 2%)', // --card (dark)
+    notification: 'hsl(5 83% 47%)', // --destructive (dark)
+    primary: 'hsl(249 100% 4%)', // --primary (same as light)
+    text: 'hsl(249 16% 97%)', // --foreground (dark)
+  },
+};
